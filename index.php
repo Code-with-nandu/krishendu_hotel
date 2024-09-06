daef8cfa94695a047a3df7b08c7c98b0e21c9ebc
krishnendu dalui <krishnendudalui@gmail.com>
2024-09-06 13:03:32 +0530

    sticki navbar

--- index.php
+++ index.php
@@ -21,9 +21,9 @@ tr<!DOCTYPE html>
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light px-lg-2 py-lg-3  shadow-sm sticky-top">
         <div class="container-fluid">
-            <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">Krishendu Dalui Hotel</a>
+            <a class="navbar-brand me-5 fw-bold fs-3 h-font " href="index.php">Krishendu Dalui Hotel</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
