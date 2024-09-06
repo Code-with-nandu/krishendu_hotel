f225d4fdc9b545fa8a308da56f05cf7ff370dfa9
krishnendu dalui <krishnendudalui@gmail.com>
2024-09-06 22:38:51 +0530

    restructuring

--- index.php
+++ index.php
@@ -1,62 +1,70 @@
-tr<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <title>ART OF liVING HOTEL</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-
-    <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <title> liVIN HOTEL</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&family=Poppins:ital,
+wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,
+600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <style>
-        * {
-            font-family: 'popins', sans-serif;
-        }
+         * {
+        font-family: 'Poppins', sans-serif;
+    }
 
-        .h-font {
-            font-family: 'Merinda' cursive;
-        }
+    .h-font {
+        font-family: 'Merienda', cursive;
+    }
     </style>
 
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light px-lg-2 py-lg-3  shadow-sm sticky-top">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
         <div class="container-fluid">
-            <a class="navbar-brand me-5 fw-bold fs-3 h-font " href="index.php">Krishendu Dalui Hotel</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.html">Livein Hotel</a>
+            <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active me-2 " aria-current="page" href="#">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link me-2" href="#">Room</a>
+                        <a class="nav-link me-2 " href="#">Rooms</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link  me-2" href="#">Facilities</a>
+                        <a class="nav-link me-2" href="#">Facilities</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link  me-2" href="#">Contact Us</a>
+                        <a class="nav-link me-2" href="#">Contact Us</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link  " href="#">About</a>
+                        <a class="nav-link" href="#">About</a>
                     </li>
-
+              
+                   
                 </ul>
                 <div class="d-flex">
                     <button class="btn btn-outline-success" type="submit">Search</button>
                 </div>
-
+              
             </div>
         </div>
     </nav>
 
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+
 </body>
 
 </html>
